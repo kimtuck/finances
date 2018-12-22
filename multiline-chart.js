fd9aeffc00767abf8dc0a3dd2be23697482acfa0
rhanson2 <Treasure2012>
2018-12-22 07:53:37 -0600

    12/22/2018

--- multiline-chart.js
+++ multiline-chart.js
@@ -3,8 +3,8 @@ function multiline_chart() {
   const lastRealData = 1;
   const data = [
     { Date: '10/15/2018', LOCFloat: '60992', LOCFixed: '19942', RobertsCar: '7726', LauriesCar: '21694', StudentLoan: '2261' },
-    { Date: '11/15/2018', LOCFloat: '61427', LOCFixed: '19905', RobertsCar: '7431', LauriesCar: '20918 ', StudentLoan: '2261' },
-    { Date: '12/15/2018', LOCFloat: '0', LOCFixed: '0', RobertsCar: '0', LauriesCar: '0', StudentLoan: '0' },
+    { Date: '11/15/2018', LOCFloat: '61427', LOCFixed: '19905', RobertsCar: '7431', LauriesCar: '21208 ', StudentLoan: '2261' },
+    { Date: '12/15/2018', LOCFloat: '63419', LOCFixed: '19865', RobertsCar: '7136', LauriesCar: '20722', StudentLoan: '2261' },
     { Date: '01/15/2019', LOCFloat: '0', LOCFixed: '0', RobertsCar: '0', LauriesCar: '0', StudentLoan: '0' },
     { Date: '06/15/2019', LOCFloat: '0', LOCFixed: '0', RobertsCar: '0', LauriesCar: '0', StudentLoan: '0' },
     { Date: '03/15/2019', LOCFloat: '0', LOCFixed: '0', RobertsCar: '0', LauriesCar: '0', StudentLoan: '0' }
